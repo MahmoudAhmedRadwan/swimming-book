6031f89b2d931993943ca810e98a21b63cd28604
AlyEbrahim27 <43121032+vamfox27@users.noreply.github.com>
2021-08-27 17:15:46 +0200

    Update SideMenu.js

--- src/components/global/sideMenu/SideMenu.js
+++ src/components/global/sideMenu/SideMenu.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './sideMenuStyle.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfo, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInfo, faEnvelope,faLaptop } from '@fortawesome/free-solid-svg-icons';
 import ProfileImg from './avatar.png'
 class SideMenu extends React.Component {
     render(){
         return(
             <div className="side-menu">
+                {/* BRAND NAME */}
                 <h2 className="brand-name">
                     Swimming
                 </h2>
+                {/* PROFILE SECTION */}
                 <div className="menu-profile">
                     <div className="user-info">
                         <div className="profile-image-section">
@@ -28,9 +30,21 @@ class SideMenu extends React.Component {
                             <h4 className="user-position">Web Developer/Designer</h4>
                         </div>
                     </div>
-                    
+                    {/* NAVIGATION SIDE MENU */}
                     <div className="menu-navigation-list">
-
+                        <ul className="menu-items">
+                            <li className="menu-item">
+                                <a href="#"><FontAwesomeIcon icon={faEnvelope} /> <span className="link-text">Menu Link</span> </a>
+                                <ul className="expaned-menu-itmes">
+                                    <li className="expanded-menu-item">
+                                        <a href="#"><span className="link-text">Expanded Menu Link</span> </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="menu-item"> 
+                                <a href="#"><FontAwesomeIcon icon={faEnvelope} /><span className="link-text"> Menu Link </span> </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
