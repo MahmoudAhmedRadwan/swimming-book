5d624d61cc9a2202857244b373644f0336a5bd96
AlyEbrahim27 <43121032+vamfox27@users.noreply.github.com>
2021-08-30 10:25:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './RtlApp.css';
 import NavBar from './components/global/navBar/NavBar';
 import SideMenu from './components/global/sideMenu/SideMenu';
+import Login from './components/global/login/Login';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 <i class="fas fa-bars"></i>
@@ -10,8 +11,10 @@ library.add(faBars)
 function App() {
   return (
     <div className="App">
+        
       <NavBar />
       <SideMenu />
+      <Login />
     </div>
   );
 }
