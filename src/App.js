22e07829772ff82f4c70d13e2a567caf7cf9f0be
Mahmoud Radwan <mahmoudradwan2625@gmail.com>
2021-08-30 11:48:30 +0200

    Log in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './RtlApp.css';
+import './RtlApp.css'; 
+import {Route, Switch, Link, BrowserRouter} from 'react-router-dom'
 import NavBar from './components/global/navBar/NavBar';
 import SideMenu from './components/global/sideMenu/SideMenu';
 import Login from './components/global/login/Login';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-<i class="fas fa-bars"></i>
+
 
 library.add(faBars)
 
@@ -14,7 +15,10 @@ function App() {
         
       <NavBar />
       <SideMenu />
-      <Login />
+
+      <BrowserRouter>
+        <Route path="/logIn" component={Login}/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/global/login/Login.js
+++ src/components/global/login/Login.js
@@ -4,8 +4,26 @@ import './Login.scss';
 class Login extends React.Component {
     render(){
         return(
-            <div className="login-page">
-                test
+            <div className="login-page-full-width">
+                <div className="login-page">
+                    <div className="log-in_container">
+                        <h3>Swimming</h3>
+                        <div className="logIn_body">
+                            <p> Please log in <span>Welcome</span></p>
+                            <input type="text" placeholder="User name" />
+                            <input type="passowrd" placeholder="Passowrd" />
+                            <div className="logIn_footer">
+                                <button className="logIn_Btn">Log In</button>
+                                <a href="Forget-Passowrd">?Forget Your Passowrd</a>
+                            </div>
+                        </div>
+                        <div className="webSite_links">
+                            <a href="#">Contact Us</a>
+                            <a href="#">Privacy</a>
+                            <a href="#">About</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
