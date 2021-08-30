3bc422672511568b77338f1dac4a7d05924c1e86
AlyEbrahim27 <43121032+vamfox27@users.noreply.github.com>
2021-08-30 10:25:33 +0200

    Create Login.js

---
+++ src/components/global/login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.scss';
+
+class Login extends React.Component {
+    render(){
+        return(
+            <div className="login-page">
+                test
+            </div>
+        )
+    }
+}
+
+export default Login;
